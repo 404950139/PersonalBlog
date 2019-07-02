243efab2fa3e6f66cea0de731b3d1a19b36541bd
shi <404950139@qq.com>
2019-07-02 15:30:22 +0800

    https://github.com/404950139/PersonalBlog.git

--- loader.js
+++ loader.js
@@ -4,13 +4,11 @@ var globalConfig = require("./config.js");
 
 var controllerSet = [];
 
-console.log(globalConfig["web_path"]);
-
 var pathMap = new Map();
-var files = fs.readdirSync(globalConfig["web_path"]);
+var files = fs.readdirSync(globalConfig.web_path);
 
 for (var i = 0 ; i < files.length ; i ++) {
-    var temp = require("./" + globalConfig["web_path"] + "/" + files[i]);
+    var temp = require("./" + globalConfig.web_path + "/" + files[i]);
     if (temp.path) {
         for (var [key, value] of temp.path) {
             if (pathMap.get(key) == null) {
